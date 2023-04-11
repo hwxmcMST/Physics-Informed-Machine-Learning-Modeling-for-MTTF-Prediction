5ec740c944ae63e0e203114eaae64791fce64c82
Hao Wu Repository <39774813+hwxmcMST@users.noreply.github.com>
2023-04-11 12:08:00 -0500

    Delete untitled.m

--- untitled.m
+++
@@ -1,6 +0,0 @@
-smodel = createpde("structural","static-solid");
-importGeometry(smodel,"Blade.stl");
-figure
-pdegplot(smodel,"FaceLabels","on","FaceAlpha",0.5)
-msh = generateMesh(smodel,"Hmax",0.01);
-pdemesh(smodel)
